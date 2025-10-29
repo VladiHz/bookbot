b72ef39e5271da813a6689211972d75e3477e8e3
VladiHz <vladimir.hernandez@gmail.com>
2025-10-29 16:10:57 -0400

    refactor: update main.py to accept book path as command line argument and added more books

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import sys
 from stats import get_num_words
 from stats import count_character_repetition
 from stats import format_report
 
 if __name__ == "__main__":
-    bookpath = 'books/frankenstein.txt'
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        bookpath = sys.argv[1]
+    #bookpath = 'books/frankenstein.txt'
     num_words = get_num_words(bookpath)
     #print(f"Found {num_words} total words")
     char_repetition = count_character_repetition(bookpath)
